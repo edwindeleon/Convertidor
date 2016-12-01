faeaf6f873e9abc54529b190a22cae34598bce7e
Edwin <mardwintrader@gmail.com>
2016-12-01 11:35:51 -0400

    Commit 1

---
+++ test/converter.js
@@ -0,0 +1,12 @@
+/**
+* Module dependencies
+*/
+var expect =  require('chai').expect;
+
+describe('Temperature Conversor', function (){
+	describe('celsius to Farenheit Conversion', function () {
+		it('Converts 100C to Farenheit', function () {
+			converter
+		})
+	})
+})
